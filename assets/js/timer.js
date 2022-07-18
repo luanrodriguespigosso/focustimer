1e3d145dadc2fef9a8a6e1084f7df911fd4fc4b6
luan <luan_naul90@hotmail.com>
2022-07-18 12:49:47 -0300

    feat: Adicionado sons nos botões

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,14 +1,17 @@
+import Sounds from "./sounds.js";
+
 export default function Timer({
   minutesDisplay,
   secondsDisplay,
   resetControls,
-  minutes,
 }) {
   let timerTimeOut;
   let minutes = Number(minutesDisplay.textContent);
 
-  function updateDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  function updateDisplay(newMinutes, seconds) {
+    newMinutes = newMinutes === undefined ? minutes : newMinutes;
+    seconds = seconds === undefined ? 0 : seconds;
+    minutesDisplay.textContent = String(newMinutes).padStart(2, "0");
     secondsDisplay.textContent = String(seconds).padStart(2, "0");
   }
 
@@ -21,11 +24,13 @@ export default function Timer({
     timerTimeOut = setTimeout(function () {
       let seconds = Number(secondsDisplay.textContent);
       let minutes = Number(minutesDisplay.textContent);
-
+      let isFinished = minutes <= 0 && seconds <= 0;
       updateDisplay(minutes, 0);
 
-      if (minutes <= 0) {
+      if (isFinished) {
         resetControls();
+        updateDisplay();
+        Sounds().kitchenTimer;
         return;
       }
 
